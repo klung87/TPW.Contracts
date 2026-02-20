cc83540e2b0cb5c4f2636ba6488bfe309d032d18
klung87 <alexander@klungerbo.net>
2026-02-20 22:51:03 +0100

    Added basics

---
+++ src/Contracts/Errors/Error.cs
@@ -0,0 +1,15 @@
+﻿namespace Contracts.Errors;
+
+public sealed class Error
+{
+    private Exception? _exception;
+    private string _message;
+    private int _code;
+
+    public Error(string message, int code, Exception? exception = null)
+    {
+        _message = message;
+        _code = code;
+        _exception = exception;
+    }
+}
---
+++ src/Contracts/Results/Result.cs
@@ -0,0 +1,37 @@
+﻿using Contracts.Errors;
+
+namespace Contracts.Results;
+
+public sealed class Result<T>
+{
+    private T? _value;
+    private IEnumerable<Error> _errors = [];
+    private readonly bool _isSuccess;
+
+    public bool IsSuccess => _isSuccess;
+
+    public Result(T item)
+    {
+        if (item is null)
+        {
+            throw new ArgumentNullException("Null is not a valid Result value");
+        }
+
+        _value = item;
+        _isSuccess = true;
+    }
+
+    public Result(IEnumerable<Error> errors)
+    {
+        _errors = errors;
+        _value = default;
+        _isSuccess = false;
+    }
+
+    private T GetValue() => _isSuccess ? _value! : throw new InvalidOperationException("Value of result is null");
+
+    public static implicit operator Result<T>(T item) => new(item);
+    public static implicit operator Result<T>(Error error) => new([error]);
+
+    public static explicit operator T(Result<T> result) => result.GetValue();
+}
